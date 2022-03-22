67230af28cb5a41ac1ff7f728de03701f6c8901e
W-Shakespeare <kiiiikkiiiik2@gmail.com>
2022-03-22 15:47:31 +0200

    Fix api, used fetch instead axios

--- src/api/api.config.js
+++ src/api/api.config.js
@@ -1,16 +1,20 @@
-import axios from "axios";
+// import axios from "axios";
 
-// axios config
-const axiosConfig = axios.create({
-  baseURL: "https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e",
-  // baseURL: "https://62322c9ac5ec1188ad2c7333.mockapi.io/cardList",
-});
+const myApi = "https://62322c9ac5ec1188ad2c7333.mockapi.io/cardList";
+// const yourApi2 = "https://run.mocky.io/v3/b7d36eea-0b3f-414a-ba44-711b5f5e528e";
 
-axiosConfig.interceptors.response.use(
-  (res) => res.data,
-  (err) => {
-    throw new Error(err.response.data.message);
-  }
-);
+const api = ({ method, data, url = "" }) =>
+  fetch(myApi + url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((res) => res.json());
 
-export default axiosConfig;
+const apiConfig = {
+  get: (data) => api({ ...data, method: "GET" }),
+  put: (data) => api({ ...data, method: "PUT" }),
+};
+
+export default apiConfig;
